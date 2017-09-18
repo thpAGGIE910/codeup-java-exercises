69b2bc72c3265b69ea06131c3e280ab3bd63a6b6
Travis Payne <thpaggie910@gmail.com>
2017-09-18 09:58:40 -0500

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    //intellij shortcut: psvm
+    public static void main(String[] args) {
+
+        //intellij shortcut: sout
+        System.out.println("Hello World!");
+
+    }
+}
