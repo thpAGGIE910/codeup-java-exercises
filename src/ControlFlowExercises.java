035dece7b4e5a4adcc8a997cca34761ef5b712d7
Travis Payne <thpaggie910@gmail.com>
2017-09-19 15:23:24 -0500

    Add Exercise 3: Table of Powers to the Control Flow Exercises

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,16 +1,28 @@
+import java.util.Scanner;
+
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        //2. FizzBuzz
-        for (byte i = 1; i <= 100; i++) {
-           if (i % 15 == 0) {
-               System.out.println("FizzBuzz");
-           } else if (i % 3 == 0) {
-               System.out.println("Fizz");
-           } else if (i % 5 == 0) {
-               System.out.println("Buzz");
-           } else {
-               System.out.println(i);
-           }
+        //3. Table of Powers
+        Scanner input = new Scanner(System.in);
+        int end = 0;
+        char runAgain = 'n';
+
+        do {
+            System.out.println("This program displays the squares and cubes of numbers from 1 to the number you enter");
+            System.out.print("What number would you like to go up to?: ");
+            end = input.nextInt();
+            input.nextLine();
+
+            System.out.println("Here is your table!\n");
+            System.out.println("number | squared | cubed");
+            System.out.println("------ | ------- | -----");
+            for (int count = 1; count <= end; count++) {
+                System.out.printf("%-6d | %-7d | %d\n", count, (int) Math.pow(count, 2), (int) Math.pow(count, 3));
+            }
+
+            System.out.print("\nWould you like to enter another number? [y/n]: ");
+            runAgain = Character.toLowerCase(input.next().charAt(0));
         }
+        while(runAgain != 'n');
     }
 }
