954b0ca3e8df15ab9af6a07774e89ad65d322c17
Travis Payne <thpaggie910@gmail.com>
2017-09-21 12:34:30 -0500

    Add exercise 2: getInteger(min, max) to the project

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,11 +1,46 @@
+import java.util.Scanner;
+
 public class MethodsExercises {
     public static void main(String[] args) {
+        //Problem 1
         System.out.printf("The sum of 1 and 3 is %d\n", add(1,3));
         System.out.printf("The difference of 5 and 2 is %d\n", subtract(5, 2));
         System.out.printf("The quotient of 6 by 3 is %d\n", divide(6,3));
         System.out.printf("The product of 9 and 5 is %d\n", multiply(9,5));
         System.out.printf("The remainder of 7 divided by 3 is %d\n", modulus(7,3));
         //System.out.printf("Trying to divide by zero, this should throw an error...", divide(1,0));
+
+        //Problem 2
+        int min = 1;
+        int max = 10;
+        int userInput;
+
+        if(min <= max) {
+            System.out.printf("Please enter a number between %d and %d: ", min, max);
+            userInput = getInteger(min, max);
+        } else {
+            System.out.printf("Please enter a number between %d and %d: ", max, min);
+            userInput = getInteger(max, min);
+        }
+
+        System.out.printf("You entered the number %d!", userInput);
+    }
+
+    public static int getInteger(int min, int max) {
+        Scanner inputStream = new Scanner(System.in);
+        int input = inputStream.nextInt();
+
+        if(input < min) {
+            System.out.printf("ERROR: The number you entered (%d) is less than the min (%d)!\n", input, min);
+            System.out.printf("Please enter a number between %d and %d: ", min, max);
+            return getInteger(min, max);
+        } else if (input > max) {
+            System.out.printf("ERROR: The number you entered (%d) is greater than the max (%d)!\n", input, max);
+            System.out.printf("Please enter a number between %d and %d: ", min, max);
+            return getInteger(min, max);
+        }
+
+        return input;
     }
 
     public static int add(int a, int b) {
