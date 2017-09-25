54112270d61a734a6182b2559c7388bc366a45f3
Travis Payne <thpaggie910@gmail.com>
2017-09-25 13:57:13 -0500

    Finish part 2 of the Object Oriented Programming exercises

--- src/Person.java
+++ src/Person.java
@@ -25,5 +25,59 @@ public class Person {
         System.out.printf("Setting my name to %s...\n", newName);
         me.setName(newName);
         System.out.printf("Testing Person sayHello() method: %s\n", me.sayHello());
+
+        // Problem 2
+        /*
+         * The String equals method checks if a string is "deep" equals to another string.
+         * The first test should return true.
+         * The double equals tests the "shallow" equality (are the memory locations equal).
+         * The second test should return false.
+         */
+        //Person person1 = new Person("John");
+        //Person person2 = new Person("John");
+        //System.out.println(person1.getName().equals(person2.getName()));
+        //System.out.println(person1 == person2);
+        /*
+         * Output:
+         * true
+         * false
+         */
+
+        /*
+         * The assignment operator assigns a copy of the reference (memory location).
+         * The statement being printed should be true
+         */
+        //Person person1 = new Person("John");
+        //Person person2 = person1;
+        //System.out.println(person1 == person2);
+        /*
+         * Output:
+         * true
+         */
+
+        /*
+         * Because the assignment assigns the reference of one object to another, the
+         * two will point to the same object. Modifying one instance has the effect of modifying both,
+         * because the two references point to the same object.
+         * the output should be the following:
+         * John
+         * John
+         * Jane
+         * Jane
+         */
+        Person person1 = new Person("John");
+        Person person2 = person1;
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+        person2.setName("Jane");
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+        /*
+         * Output:
+         * John
+         * John
+         * Jane
+         * Jane
+         */
     }
 }
