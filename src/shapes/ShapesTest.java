70ed0e77839f7aedbc038bac96073bfe1ec83543
Travis Payne <thpaggie910@gmail.com>
2017-09-28 18:11:22 -0500

    Add answers for the last two questions of the exercises to the ShapesTest file

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -12,3 +12,14 @@ public class ShapesTest {
         System.out.printf("Perimeter = %f Area = %f\n", shape.getPerimeter(), shape.getArea());
     }
 }
+
+/*
+ * 9. Why does the code fail to compile if you leave off the getPerimeter method in Rectangle?
+ * Ans: Every class that implements an interface must implement each method declared in the interface. Since
+ * the Rectangle class implements the Measurable interface, it must define the implementation of the getPerimeter()
+ * method.
+ *
+ * 10. What happens if you try to call the getLength or getWidth methods of the myShape variable? Why?
+ * Ans: Because the Measurable interface does not declare the getLength or getWidth methods, it will not be available
+ * to any instances that are declared type Measurable.
+ */
